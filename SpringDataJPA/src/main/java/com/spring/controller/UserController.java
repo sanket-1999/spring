a554b27608109110b5c4d3178d7a15c0e6af054e
sanket-1999 <60914794+sanket-1999@users.noreply.github.com>
2020-04-13 22:40:17 +0530

    user

---
+++ SpringDataJPA/src/main/java/com/spring/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.json.User;
+import com.spring.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@Autowired
+	private UserService registrationService;
+
+	@GetMapping("/get")
+	public List<User> getAllUsers() {
+		return registrationService.getAllUsers();
+	}
+
+	@PostMapping(value="/register", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody User registerUser(@RequestBody User user) {
+		return registrationService.save(user);
+	}
+
+	/*
+	 @PutMapping(value="/user/{id}", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody User updateUser(@RequestBody User user, @PathVariable(value="id") String id) {
+		return registrationService.update(user, id);
+	}
+	
+
+	@DeleteMapping(value="/user/{id}")
+	public boolean deleteUser(@PathVariable(value="id") String id) {
+		return registrationService.delete(id);
+	}
+
+	*/
+	
+	@PostMapping(value="/login",consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
+	public String login(@RequestBody User user){
+		return registrationService.login(user);
+	}
+	
+	@DeleteMapping(value="/logout",produces=MediaType.APPLICATION_JSON_VALUE)
+	public User logoutUser(@RequestHeader(name="auth-token") String auth) {
+		return registrationService.logout(auth);
+	}
+	
+	
+}
