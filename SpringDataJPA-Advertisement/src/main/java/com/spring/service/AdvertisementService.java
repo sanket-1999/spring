c6f4f0caa6c77553346040addeac4f9e63e3af2f
sanket-1999 <60914794+sanket-1999@users.noreply.github.com>
2020-04-16 22:09:31 +0530

    Add files via upload

---
+++ SpringDataJPA-Advertisement/src/main/java/com/spring/service/AdvertisementService.java
@@ -0,0 +1,31 @@
+package com.spring.service;
+
+import java.util.List;
+
+import com.spring.json.Advertisement;
+
+public interface AdvertisementService {
+
+	public Advertisement save(Advertisement advertisement, String akey);
+
+	public String updateadvertisement(Advertisement advertisement, String key);
+
+	public List<String> getCategory();
+
+	public List<Advertisement> getBySearch(String searchText);
+
+	public List<Advertisement> getall();
+
+	public List<String> returnActions();
+
+	public List<Advertisement> AdvertiseAsc();
+
+	public List<Advertisement> AdvertiseDesc();
+
+	public List<Advertisement> AdvertiseAscPostid();
+
+	public List<Advertisement> AdvertiseDescPostid();
+
+	public String deleteById(String akey, String postId);
+
+}
