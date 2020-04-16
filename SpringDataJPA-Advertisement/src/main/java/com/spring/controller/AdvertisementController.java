2a95d20a048d4004d163c1f7c6e2200101d9e909
sanket-1999 <60914794+sanket-1999@users.noreply.github.com>
2020-04-16 08:44:52 +0530

    ..

--- SpringDataJPA-Advertisement/src/main/java/com/spring/controller/AdvertisementController.java
+++ SpringDataJPA-Advertisement/src/main/java/com/spring/controller/AdvertisementController.java
@@ -21,48 +21,41 @@ import com.spring.json.User;
 import com.spring.service.AdvertisementService;
 import com.spring.service.UserService;
 
-
 @RestController
 @RequestMapping("/adv")
 public class AdvertisementController {
-	
+
 	@Autowired
 	private AdvertisementService advertisementService;
 
-	@PostMapping(value="/post",consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
-	public Advertisement addAdvertisement(@RequestBody Advertisement advertisement,@RequestHeader String akey) {
-		return advertisementService.save(advertisement,akey);
+	@PostMapping(value = "/post", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Advertisement addAdvertisement(@RequestBody Advertisement advertisement, @RequestHeader String akey) {
+		return advertisementService.save(advertisement, akey);
 	}
-	
-	
 
-	@PutMapping(value="/update", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
-	public String updateAdvertisement(@RequestBody Advertisement advertisement,@RequestHeader String akey) {
-		return advertisementService.updateadvertisement(advertisement,akey);
+	@PutMapping(value = "/update", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String updateAdvertisement(@RequestBody Advertisement advertisement, @RequestHeader String akey) {
+		return advertisementService.updateadvertisement(advertisement, akey);
 	}
-	
-	
-	@GetMapping(value="/getall",produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+
+	@GetMapping(value = "/getall", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public List<Advertisement> getall() {
-		 return advertisementService.getall();
+		return advertisementService.getall();
 	}
-	
-	@GetMapping(value="/getcategory",produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+
+	@GetMapping(value = "/getcategory", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public List<String> getCategory() {
-		 return advertisementService.getCategory();
+		return advertisementService.getCategory();
 	}
-	
-	@GetMapping(value="/getbysearch/{searchText}",produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
-	public List<Advertisement> getBySearch(@PathVariable(name="searchText") String searchText){
-		 return advertisementService.getBySearch(searchText);
+
+	@GetMapping(value = "/getbysearch/{searchText}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public List<Advertisement> getBySearch(@PathVariable(name = "searchText") String searchText) {
+		return advertisementService.getBySearch(searchText);
 	}
-	
-	@GetMapping(value="/actions",produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
-	public List<String> returnActions(){
-		 return advertisementService.returnActions();
+
+	@GetMapping(value = "/actions", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public List<String> returnActions() {
+		return advertisementService.returnActions();
 	}
-	
-	
-	
+
 }
-	
